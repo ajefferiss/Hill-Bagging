9a559f05046b014446545f1256243d1282dda0cc
Adam Jefferiss <adam.jefferiss@gmail.com>
2020-05-17 16:25:15 +0100

    Change format of date in toast and reload dialog on change

--- app/src/main/java/uk/co/openmoments/hillbagging/ui/adapters/HillsAdapter.java
+++ app/src/main/java/uk/co/openmoments/hillbagging/ui/adapters/HillsAdapter.java
@@ -1,11 +1,13 @@
 package uk.co.openmoments.hillbagging.ui.adapters;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.text.Html;
 import android.text.SpannedString;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import uk.co.openmoments.hillbagging.R;
@@ -34,6 +39,8 @@ public class HillsAdapter extends EmptyRecyclerView.Adapter<HillsViewHolder> imp
     private int currentHillPosition;
     private List<HillsWithWalked> mHillsDataSet;
     private final static String MAPS_URI = "https://www.google.com/maps/@?api=1&map_action=map&center=%s,%s&basemap=terrain";
+    private AlertDialog alertDialog;
+    private View tempView;
 
     public HillsAdapter(Context context) {
         this.context = context;
@@ -50,6 +57,7 @@ public class HillsAdapter extends EmptyRecyclerView.Adapter<HillsViewHolder> imp
     public void onBindViewHolder(@NonNull HillsViewHolder holder, int position) {
         Hill hill = mHillsDataSet.get(position).hill;
         HillsWalked hillsWalked = mHillsDataSet.get(position).hillsWalked;
+        tempView = null;
 
         String hillWalkedDate = "";
         if (hillsWalked != null) {
@@ -66,56 +74,10 @@ public class HillsAdapter extends EmptyRecyclerView.Adapter<HillsViewHolder> imp
         holder.setWalkedDate(Html.fromHtml(tempText, Html.FROM_HTML_MODE_LEGACY));
 
         // Make the hill walked date effectively final...
-        String finalHillWalkedDate = hillWalkedDate;
         holder.setItemLongClickListener((view, pos) -> {
             currentHillPosition = pos;
-            final AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-            final View dialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.dialog_hill_details, null);
-
-            if (hillsWalked != null) {
-                dialogView.findViewById(R.id.mark_walked_linear_layout).setVisibility(View.GONE);
-            }
-
-            String tempText1;
-            TextView tempTextView = dialogView.findViewById(R.id.hill_dialog_name);
-            tempTextView.setText(hill.getName());
-
-            tempTextView = dialogView.findViewById(R.id.hill_dialog_height);
-            tempText1 = view.getContext().getString(R.string.hill_walked_height_desc, hill.getMetres(), hill.getFeet());
-            tempTextView.setText(Html.fromHtml(tempText1, Html.FROM_HTML_MODE_LEGACY));
-
-            tempTextView = dialogView.findViewById(R.id.hill_dialog_location);
-            tempText1 = view.getContext().getString(R.string.hill_dialog_position, hill.getLatitude(), hill.getLongitude());
-            tempTextView.setText(Html.fromHtml(tempText1, Html.FROM_HTML_MODE_LEGACY));
-
-            tempTextView = dialogView.findViewById(R.id.hill_dialog_walked_date);
-            tempText1 = view.getContext().getString(R.string.hill_walked_date_desc, finalHillWalkedDate);
-            tempTextView.setText(Html.fromHtml(tempText1, Html.FROM_HTML_MODE_LEGACY));
-
-            Button hillButton = dialogView.findViewById(R.id.hill_dialog_view_map);
-            hillButton.setOnClickListener(v -> {
-                Uri gmmIntentUri = Uri.parse(String.format(MAPS_URI, hill.getLatitude(), hill.getLongitude()));
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                mapIntent.setPackage("com.google.android.apps.maps");
-                context.startActivity(mapIntent);
-            });
-
-            hillButton = dialogView.findViewById(R.id.hill_dialog_view_higgbagging_entry);
-            hillButton.setOnClickListener(v -> {
-                Intent i = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(hill.getHillURL()));
-                context.startActivity(i);
-            });
-
-            hillButton = dialogView.findViewById(R.id.hill_marked_walked_btn);
-            hillButton.setOnClickListener(v -> {
-                FragmentManager fragmentManager = ((AppCompatActivity) context).getSupportFragmentManager();
-                DatePickerFragment dateFragment = new DatePickerFragment();
-                dateFragment.setCallback(HillsAdapter.this);
-                dateFragment.show(fragmentManager, "datePicker");
-            });
-
-            builder.setView(dialogView).setNegativeButton(R.string.close, (dialog, which) -> dialog.cancel());
-            builder.show();
+            tempView = view;
+            setupDialogView(view, hill, hillsWalked);
         });
     }
 
@@ -133,6 +95,7 @@ public class HillsAdapter extends EmptyRecyclerView.Adapter<HillsViewHolder> imp
         notifyDataSetChanged();
     }
 
+    @SuppressLint("SimpleDateFormat")
     @Override
     public void receiveResult(String value) {
         AppDatabase database = AppDatabase.getDatabase(context);
@@ -142,6 +105,68 @@ public class HillsAdapter extends EmptyRecyclerView.Adapter<HillsViewHolder> imp
         database.hillWalkedDAO().insertAll(hillWalked);
 
         String hillName = mHillsDataSet.get(currentHillPosition).hill.getName();
-        Toast.makeText(context, "Walked " + hillName + "on " + value, Toast.LENGTH_LONG).show();
+
+        String walkedDate = value;
+        try {
+            Date date = new SimpleDateFormat("yyyy-MM-dd").parse(value);
+            walkedDate = (date != null) ? new SimpleDateFormat("dd-MM-yyyy").format(date) : value;
+        } catch (ParseException pe) {
+            Log.e(HillsAdapter.class.getSimpleName(), "Failed to parse walked date " + value, pe);
+        }
+
+        alertDialog.dismiss();
+        setupDialogView(tempView, mHillsDataSet.get(currentHillPosition).hill, hillWalked);
+
+        Toast.makeText(context, "Walked " + hillName + "on " + walkedDate, Toast.LENGTH_LONG).show();
+    }
+
+    private void setupDialogView(View view, Hill hill, HillsWalked hillsWalked) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        final View dialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.dialog_hill_details, null);
+
+        dialogView.findViewById(R.id.mark_walked_linear_layout).setVisibility((hillsWalked != null) ? View.GONE : View.VISIBLE);
+        String hillWalkedDate = (hillsWalked != null) ? hillsWalked.getWalkedDate().toString() : "";
+
+        String tempText1;
+        TextView tempTextView = dialogView.findViewById(R.id.hill_dialog_name);
+        tempTextView.setText(hill.getName());
+
+        tempTextView = dialogView.findViewById(R.id.hill_dialog_height);
+        tempText1 = view.getContext().getString(R.string.hill_walked_height_desc, hill.getMetres(), hill.getFeet());
+        tempTextView.setText(Html.fromHtml(tempText1, Html.FROM_HTML_MODE_LEGACY));
+
+        tempTextView = dialogView.findViewById(R.id.hill_dialog_location);
+        tempText1 = view.getContext().getString(R.string.hill_dialog_position, hill.getLatitude(), hill.getLongitude());
+        tempTextView.setText(Html.fromHtml(tempText1, Html.FROM_HTML_MODE_LEGACY));
+
+        tempTextView = dialogView.findViewById(R.id.hill_dialog_walked_date);
+        tempText1 = view.getContext().getString(R.string.hill_walked_date_desc, hillWalkedDate);
+        tempTextView.setText(Html.fromHtml(tempText1, Html.FROM_HTML_MODE_LEGACY));
+
+        Button hillButton = dialogView.findViewById(R.id.hill_dialog_view_map);
+        hillButton.setOnClickListener(v -> {
+            Uri gmmIntentUri = Uri.parse(String.format(MAPS_URI, hill.getLatitude(), hill.getLongitude()));
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+            context.startActivity(mapIntent);
+        });
+
+        hillButton = dialogView.findViewById(R.id.hill_dialog_view_higgbagging_entry);
+        hillButton.setOnClickListener(v -> {
+            Intent i = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(hill.getHillURL()));
+            context.startActivity(i);
+        });
+
+        hillButton = dialogView.findViewById(R.id.hill_marked_walked_btn);
+        hillButton.setOnClickListener(v -> {
+            FragmentManager fragmentManager = ((AppCompatActivity) context).getSupportFragmentManager();
+            DatePickerFragment dateFragment = new DatePickerFragment();
+            dateFragment.setCallback(HillsAdapter.this);
+            dateFragment.show(fragmentManager, "datePicker");
+        });
+
+        builder.setView(dialogView).setNegativeButton(R.string.close, (dialog, which) -> dialog.cancel());
+        alertDialog = builder.create();
+        alertDialog.show();
     }
 }
